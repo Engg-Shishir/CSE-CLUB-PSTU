d5a521a7115b1b17b4b1122473b56a600984a5c0
Shishir Bhuiyan <shishir.cse.pstu@gmail.com>
2023-07-02 18:18:20 +0600

    Create Navbar,Footer Component

---
+++ views/layout/footer.php
@@ -0,0 +1,55 @@
+<div class="row card">
+  <footer>
+    <div class="footer-top">
+      <div class="footer-top-box">
+        <div class="footer-top-box-right">
+          <p>Follow Us</p>
+          <div class="social">
+            <ul>
+              <li>
+                <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+              </li>
+              <li>
+                <a href="#"><i class="fa-brands fa-twitter"></i></a>
+              </li>
+              <li>
+                <a href="#"><i class="fa-brands fa-youtube"></i></a>
+              </li>
+              <li>
+                <a href="#"><i class="fa-brands fa-discord"></i></a>
+              </li>
+              <li>
+                <a href="#"><i class="fa-brands fa-linkedin-in"></i></a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="footer-top-box-left">
+          <div class="footer-logo-box">
+            <img src="./Image/Club-Logo-Blue.svg" alt="" />
+          </div>
+          <div class="aboutText">
+            <p>
+              CSE club is one of the largest scientific clubs in Algeria.
+              Working since 2008, our main goal is to offer original and
+              innovative content throughout our hackathons, workshops,
+              training and social media.
+            </p>
+          </div>
+          <ul>
+            <li><a href="">Privacy Policy</a></li>
+            <li><a href="">Terms & Condition</a></li>
+            <li><a href="">Contact</a></li>
+            <li><a href="">Blog</a></li>
+            <li><a href="">FAQ</a></li>
+            <li><a href="">Developers</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <hr />
+    <div class="footer-botom">
+      <p>© 2023 CSE CLUB PSTU. All rights reserved.</p>
+    </div>
+  </footer>
+</div>
---
+++ views/layout/navbar.php
@@ -0,0 +1,117 @@
+
+<div class="container">
+  <div class="engg-shishir-navbar">
+    <div class="engg-shishir-nav-left">
+      <a href="<?= url('/'); ?>"><img src="<?= assets('image/Club-Logo-Blue.svg') ?>" alt="" /></a>
+    </div>
+    <div class="engg-shishir-nav-right">
+      <a target="_blank" href="./events" class="engg-shishir-nav-right-event">It carnival 2023</a>
+      <a href="" class="engg-shishir-nav-right-login">Login</a>
+      <div class="engg-shishir-nav-right-menu" onclick="hamburger()">
+        <span class="text">MENU</span>
+        <span class="hamburger">
+          <i class="line"></i>
+          <i class="line"></i>
+          <i class="line"></i>
+        </span>
+      </div>
+    </div>
+  </div>
+  <div class="engg-shishir-fullnav">
+    <div class="engg-shishir-fullnav-menu">
+      <div class="tabbar">
+        <ul class="tabs-menu">
+          <li class="tab-li tab-li-active">Programs</li>
+          <li class="tab-li">More</li>
+        </ul>
+      </div>
+      <div class="tab-content">
+        <ul class="tab-li-content engg-shishir-fullnav-left-ul tab-li-content-active">
+          <li class="shishir-navitem">
+            <a target="_blank" href="./events" class="shishir-navLink">
+              <p>IT Carinival 2023</p>
+              <p>IT Carinival 2023</p>
+            </a>
+          </li>
+          <li class="shishir-navitem">
+            <a href="" class="shishir-navLink">
+              <p>UI UX Development</p>
+              <p>UI UX Development</p>
+            </a>
+          </li>
+          <li class="shishir-navitem">
+            <a href="" class="shishir-navLink">
+              <p>Cyber Security</p>
+              <p>Cyber Security</p>
+            </a>
+          </li>
+          <li class="shishir-navitem">
+            <a href="" class="shishir-navLink">
+              <p>Digital Marketing</p>
+              <p>Digital Marketing</p>
+            </a>
+          </li>
+          <li class="shishir-navitem">
+            <a href="" class="shishir-navLink">
+              <p>ICPC Bootcamp</p>
+              <p>ICPC Bootcamp</p>
+            </a>
+          </li>
+        </ul>
+        <ul class="tab-li-content engg-shishir-fullnav-right-ul">
+          <li class="shishir-navitem">
+            <a href="" class="shishir-navLink">
+              <p>Alumini</p>
+              <p>Alumini</p>
+            </a>
+          </li>
+          <li class="shishir-navitem">
+            <a href="" class="shishir-navLink">
+              <p>Executive Member</p>
+              <p>Executive Member</p>
+            </a>
+          </li>
+          <li class="shishir-navitem">
+            <a href="" class="shishir-navLink">
+              <p>partners</p>
+              <p>partners</p>
+            </a>
+          </li>
+          <li class="shishir-navitem">
+            <a href="" class="shishir-navLink">
+              <p>About</p>
+              <p>About</p>
+            </a>
+          </li>
+          <li class="shishir-navitem">
+            <a href="" class="shishir-navLink">
+              <p>Contact</p>
+              <p>Contact</p>
+            </a>
+          </li>
+          <li class="shishir-navitem">
+            <a href="" class="shishir-navLink">
+              <p>FAQ</p>
+              <p>FAQ</p>
+            </a>
+          </li>
+          <li class="shishir-navitem">
+            <a href="" class="shishir-navLink">
+              <p>Blog</p>
+              <p>Blog</p>
+            </a>
+          </li>
+          <li class="shishir-navitem">
+            <a href="" class="shishir-navLink">
+              <p>Suport Us</p>
+              <p>Suport Us</p>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="engg-shishir-fullnav-content">
+      <!-- <button class="closenav"  onclick="hamburger()">Close X</button> -->
+    </div>
+  </div>
+</div>
