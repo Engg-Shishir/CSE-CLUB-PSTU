eb946983eae0ceb9780992cb7d4d261980b9e2d8
Shishir Bhuiyan <shishir.cse.pstu@gmail.com>
2023-07-03 16:22:45 +0600

    update Auth & Guest Middleware

--- app/Middlewares/Auth.php
+++ app/Middlewares/Auth.php
@@ -8,15 +8,21 @@ class Auth implements IMiddleware
 {
 	public function handle(Request $request) : void
 	{
-		if (strpos($_SESSION['auth'], shishirEnv("APP_KEY")) !== false){
+		// if (strpos($_SESSION['auth'], shishirEnv("APP_KEY")) == true){
+
+		// }else{
+
+		// }
+		// $_SESSION['auth'] = shishirEnv("APP_KEY");
+	 if(isset($_SESSION['auth']) && !empty($_SESSION['auth'])) {
 			$request->authenticated = true;
-		}else{
-			$url = $_ENV["APP_URL"]."/".$_ENV["BASE_URL"]."/login";
-			?><script type="text/javascript">
-				window.location.href = "<?= $url; ?>";
-			</script><?php
-			die();
-		}
+	 }else{
+		$url = $_ENV["APP_URL"]."/".$_ENV["BASE_URL"];
+		?><script type="text/javascript">
+			window.location.href = "<?= $url; ?>";
+		</script><?php
+		die();
+	 }
 	}
 }
 
