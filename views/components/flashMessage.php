4f99095b3c3e477e8be04edfdc07439cd1367563
Shishir Bhuiyan <shishir.cse.pstu@gmail.com>
2023-07-30 01:08:44 +0600

    updated flash message

--- views/components/flashMessage.php
+++ views/components/flashMessage.php
@@ -1,11 +1,35 @@
-<?php 
+<?php
 if (isset($_SESSION["error_message"])) {
-  echo '<div class="row row-input"><div class="alert alert-warning alert-dismissible fade show" role="alert"><strong>' . $_SESSION["error_message"] . '</strong><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div></div>';
+ ?>
+  <div
+    style="padding:10px;background-color:#6c002b;color:#fff;width:100%;margin-bottom:10px;display:flex;align-items:center;justify-content:space-between;font-size:17px">
+    <strong><?= $_SESSION["error_message"]; ?></strong>
+    <span
+      style="margin-left:15px;color:#fff;font-weight:700;float:right;font-size:22px;line-height:20px;cursor:pointer;transition:.3s"
+      onclick="this.parentElement.style.display='none';" onMouseOver="this.style.color='black'"
+      onMouseOut="this.style.color='white'">&times;</span>
+  </div>
+
+<?php
   unset($_SESSION["error_message"]);
-} 
+}
 
 if (isset($_SESSION["success_message"])) {
-  echo '<div class="row row-input"><div class="alert alert-success alert-dismissible fade show" role="alert"><strong>' . $_SESSION["success_message"] . '</strong><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div></div>';
-  unset($_SESSION["success_message"]);
+?>
+<div
+  style="padding:10px;background-color:#418040;color:#fff;width:100%;margin-bottom:10px;display:flex;align-items:center;justify-content:space-between;font-size:17px">
+  <strong><?= $_SESSION["success_message"]; ?></strong>
+  <span
+    style="margin-left:15px;color:#fff;font-weight:700;float:right;font-size:22px;line-height:20px;cursor:pointer;transition:.3s"
+    onclick="this.parentElement.style.display='none';" onMouseOver="this.style.color='black'"
+    onMouseOut="this.style.color='white'">&times;</span>
+</div>
+
+<?php
+ unset($_SESSION["success_message"]);
 }
 ?>
+
+
+
+
