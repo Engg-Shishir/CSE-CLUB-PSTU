1086d882cb2f60cb4edd7b274e5189e748e3b975
Shishir Bhuiyan <shishir.cse.pstu@gmail.com>
2023-08-31 02:42:35 +0600

    Convert project section into Achivements section

--- views/Frontend/Home/projects.php
+++ views/Frontend/Home/projects.php
@@ -1,10 +1,11 @@
 <div class="row projects-row">
   <div class="projects-row-left">
-    <h2>CSE's Projects</h2>
+    <h2>Our Achivements</h2>
     <p>
       <?= $projects["text"] ?>
     </p>
-    <a href="https://github.com/Engg-Shishir" target="_blank">
+    <a href="" class="text-info">Cheeck All</a>
+    <!-- <a href="https://github.com/Engg-Shishir" target="_blank">
       <button class="glowing-btn">
         <span></span>
         <span></span>
@@ -12,7 +13,7 @@
         <span></span>
         Check all
       </button>
-    </a>
+    </a> -->
   </div>
   <div class="projects-row-right">
     <div class="projects-row-right-box owl-carousel owl-theme">
@@ -21,8 +22,6 @@
         if($value["status"]==1){ ?>
             <div class="projects-row-right-box-item">
               <img src="<?= assets('Upload/Projects/'.$value["logo"]) ?>" alt="" />
-              <a href="<?= $value["sourcecode"] ?>" target="_blank" style="color:blue !important; font-size:16px; font-weight:400;"><?= $value["title"] ?></a>
-              <p><?= $value["description"] ?></p>
             </div>
         <?php }
       } ?>
